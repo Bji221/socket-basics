cfbd2bcf0eab23ad6ef1d478b07a55ee6ad6d8c0
Bji221 <bbala.ji20@gmail.com>
2016-03-04 20:28:33 -0500

    added front end

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,28 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('connected to server');
 });
+
+socket.on('message', function(message){
+	console.log('new Message:');
+	console.log(message.text);
+
+});
+
+// Handles submitting of new messages!
+//selects all the message-form #-id elements
+var $form = jQuery('#message-form');
+
+//submit event - all browsers knows about it
+$form.on('submit', function (event){
+	//prevenDefault prevents the old-fashioned way!
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+
+
+});
--- server.js
+++ server.js
@@ -7,8 +7,24 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (){ 
+io.on('connection', function (socket){ 
 	console.log('User connected via socket.io');
+
+	//event - emits an event - name and data to send
+	
+	socket.on('message', function(message){
+		console.log('Message Received  :' + message.text);
+		//broadcast message to everybody but the sender
+		socket.broadcast.emit('message', message);
+
+	});
+
+	socket.emit('message', {
+		text : 'welcome to the Bchat'
+	});
+
+
+
 });
 
 http.listen(PORT, function(){
